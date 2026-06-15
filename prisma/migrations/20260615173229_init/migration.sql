23207a42281481b13f92aa8a34dadd30ffddcbd4
João Vítor P. Santos <48125062+joaovitorps@users.noreply.github.com>
2026-06-15 16:14:57 -0300

    build(prisma): update database tooling

--- prisma/migrations/20260615173229_init/migration.sql
+++ prisma/migrations/20260615173229_init/migration.sql
@@ -1,9 +1,12 @@
 -- CreateTable
 CREATE TABLE "Org" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
 
     CONSTRAINT "Org_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Org_name_phone_key" ON "Org"("name", "phone");
